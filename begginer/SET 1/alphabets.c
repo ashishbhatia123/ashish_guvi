7a27bd842ac5b979399ebc99a343aa8f89896256
ashishbhatia12 <ashishbhatia0209@gmail.com>
2018-01-13 16:44:18 +0530

    guvi

---
+++ begginer/SET 1/alphabets.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{char n;
+scanf("%c",&n);
+if((int)(c)>=65 && (int)(c)<=90 || (int)(c)>=97 && (int)(c)<=122)
+printf("Alphabet");
+else
+printf("No");
+}
---
+++ begginer/SET 1/bigger_number.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+scanf("%d%d%d",&a,&b,&c);
+if(a>b && a>c) { printf ("%d",a);}
+if(c>b && c>a) { printf ("%d",c);}
+if(b>a && b>c) { printf ("%d",b);}
+return(0);
+}
